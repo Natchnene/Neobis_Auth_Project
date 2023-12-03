0064f8cb7eff15cc8f338c8b5650d47dc659299d
Natchnene <kondratsiuk.marina@gmail.com>
2023-12-03 20:57:31 +0300

    Redefine models UserManager, User

--- Lorby/Auth/models.py
+++ Lorby/Auth/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
 
-# Create your models here.
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, password, **extra_fields):
+        if not username:
+            raise TypeError('Users should have an username')
+        if not email:
+            raise TypeError('Users should have an email')
+        if not password:
+            raise TypeError('Users should have a password')
+        email = self.normalize_email(email)
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(username, email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=250, unique=True)
+    email = models.EmailField(max_length=250, unique=True)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
+
+
