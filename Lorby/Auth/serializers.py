41db9a0cf03fbc83a39277c35d8ad8bbcdbad3be
Marina <45893191+Natchnene@users.noreply.github.com>
2023-12-06 16:22:24 +0300

    Auth/endpoints (#4)
    * create renderer for user
    
    * Create RegisterView

--- Lorby/Auth/serializers.py
+++ Lorby/Auth/serializers.py
@@ -5,24 +5,40 @@ from .models import User
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(min_length=8, max_length=15, write_only=True)
+    password_check = serializers.CharField(min_length=8, max_length=15, write_only=True)
 
     default_error_messages = {
-        'username': 'The username should only contain only alphabetic characters'
+        'username': 'The username should only contain only alphabetic characters.',
+        'password_mismatch': 'The two password fields did not match.'
     }
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password', 'password_check']
 
-    def validate(self, attrs):
-        email = attrs.get('email', '')
-        username = attrs.get('username', '')
+    def validate(self, data):
+        username = data.get('username', '')
 
         if not username.isalpha():
             raise serializers.ValidationError(
-                self.default_error_messages
+                self.default_error_messages['username']
             )
-        return attrs
+
+        if data['password'] != data ['password_check']:
+            raise serializers.ValidationError(
+                self.default_error_messages['password_mismatch']
+            )
+
+        return data
 
     def create(self, validated_data):
+        validated_data.pop('password_check', '')
         return User.objects.create_user(**validated_data)
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=100)
+    password = serializers.CharField(min_length=8, max_length=15, write_only=True)
+    tokens = serializers.SerializerMethodField()
+
+
--- Lorby/Auth/views.py
+++ Lorby/Auth/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions, status, views
+from rest_framework.response import Response
+from .models import User
+from .serializers import RegisterSerializer
+from .renderers import UserJSONRenderer
 
-# Create your views here.
+
+class RegisterView(generics.GenericAPIView):
+
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+    renderer_classes = (UserJSONRenderer,)
+
+    def post(self, request):
+        user = request.data
+        serializer = self.serializer_class(data=user)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
