45c3057262aa9702a530e6d354e4082becde0f55
Marina <45893191+Natchnene@users.noreply.github.com>
2023-12-08 21:34:45 +0300

    Develop (#6)
    * create virtual environment
    
    * create skeleton of project
    
    * Auth/models (#2)
    
    * Redefine models UserManager, User
    
    * Install settings for DRF, JWT. Add DRF, JWT to poetry
    
    * Create register serializer (#3)
    
    * Auth/endpoints (#4)
    
    * create renderer for user
    
    * Create RegisterView
    
    * Create confirmation email
    
    * Create serializer, endpoint for verify email
    
    * Create login and logout serializers, endpoints, urls
    
    * Corect mistake
    
    * Connect swagger
    
    * Add python-dotenv to poetry
    
    * Remove sensitive data from settings
    
    * change settings

--- Lorby/Lorby/settings.py
+++ Lorby/Lorby/settings.py
@@ -23,6 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+load_dotenv()
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
@@ -146,7 +148,6 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
 }
 
-load_dotenv()
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
