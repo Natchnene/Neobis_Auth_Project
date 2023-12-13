0d5a28a299f9f2a19ef751127168308148d14383
Marina <45893191+Natchnene@users.noreply.github.com>
2023-12-13 16:32:53 +0300

    Update views.py

--- Lorby/Auth/views.py
+++ Lorby/Auth/views.py
@@ -28,7 +28,7 @@ class RegisterView(generics.GenericAPIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    send_email = send_confirmation_email
+    send_confirmation_email = send_confirmation_email
 
 
 class VerifyEmail(views.APIView):
